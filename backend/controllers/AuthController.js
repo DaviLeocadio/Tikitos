8e59550bc5cae1d913939ebbf0cf941c3e2c11db
DaviLeocadio <davi.leocadio.sen@gmail.com>
2025-10-01 16:46:27 -0300

    fix: backend

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,31 +1,34 @@
-import jwt from 'jsonwebtoken';
-import { read, compare } from '../config/database.js';
-import { JWT_SECRET } from '../config/jwt.js';
+import jwt from "jsonwebtoken";
+import { read, compare } from "../config/database.js";
+import { JWT_SECRET } from "../config/jwt.js";
 
 const loginController = async (req, res) => {
-  const { email, senha } = req.body;
+  const { login, senha } = req.body;
 
   try {
-    const usuario = await read('usuarios', `email = '${email}'`);
+    const usuario = await read(
+      "usuarios",
+      `email = '${login}'` || `cpf = ${login}` || `id = ${login}`
+    );
 
     if (!usuario) {
-      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+      return res.status(404).json({ mensagem: "Usuário não encontrado" });
     }
 
     const senhaCorreta = await compare(senha, usuario.senha);
 
     if (!senhaCorreta) {
-      return res.status(404).json({ mensagem: 'Senha incorreta' });
+      return res.status(404).json({ mensagem: "Senha incorreta" });
     }
 
     const token = jwt.sign({ id: usuario.id, tipo: usuario.tipo }, JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
-    res.json({ mensagem: 'Login realizado com sucesso', token });
+    res.json({ mensagem: "Login realizado com sucesso", token });
   } catch (err) {
-    console.error('Erro ao fazer login', err);
-    res.status(500).json({ mensagem: 'Erro ao fazer login' });
+    console.error("Erro ao fazer login", err);
+    res.status(500).json({ mensagem: "Erro ao fazer login" });
   }
 };
 
